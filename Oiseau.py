83b2b7a732b01ec4a802079dac4748ff18c2ad68
Your Name <dmbogning15@gmail.com>
2021-03-31 16:30:45 +0200

    add Oiseau and Serpent sub_class

--- Oiseau.py
+++ Oiseau.py
@@ -13,31 +13,16 @@ class Animal(): # créer une classe Animal
         pass 
     
 
-class Serpent(Animal):  # classe serpent qui hérite de la classe animal
-    def se_deplacer(self):    #redefinir la méthode se_deplacer( ) pour serpent.
-       return 'Je rampe'
-
 class Oiseau(Animal):  # classe Oiseau qui hérite de la classe animal
     def se_deplacer(self):    #redefinir la méthode se_deplacer( ) pour Oiseau.
         return 'Je vole'
 
     
 
-
-
-#instanciez cet objet. Faites un print de ses attributs.  
-python = Animal(80, 2) 
-print(f"Le poids de l'animal est de : {python.animal_poids}")
-print(f"La taille de l'animal est de : {python.animal_taille}")
-
-
 #Instanciez maintenant ces deux objets dans le
 #programme principal et appelez la méthode se_deplacer( ) pour chacun de ces
 #objets.
-#obj_animal = Animal()
-python= Serpent(10, 5)
 rosignol = Oiseau(2, 1)
-print(f"Je suis monsieur python, {type(python)}, {python.se_deplacer()}")
 print(f"Je suis madame la rosignol, l'{type(rosignol)}, {rosignol.se_deplacer()}")
 
 
