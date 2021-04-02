d7a40e17ccf4fadf46e5437878d94471efa94787
Your Name <dmbogning15@gmail.com>
2021-04-02 14:24:35 +0200

    Operator overloading: rajouter un support pour l’opération “+”entre objets de type Zoo.

--- zoo.py
+++ zoo.py
@@ -5,36 +5,46 @@ from Animal import Animal
 
 
 class Zoo:
-    liste_animaux_zoo1=[]
-    liste_animaux_zoo2=[]
     liste_animaux_zoo=[]
-    liste_animaux_zoo=liste_animaux_zoo1 + liste_animaux_zoo2
-    
     def __init__(self, liste_animaux_zoo):
         self.liste_animaux_zoo=[]
         for elm_Animal in liste_animaux_zoo:
             if isinstance(elm_Animal, Animal) == False:
                 raise TypeError()
             else:
-                self.liste_animaux_zoo.append(str(Animal))
-               
+                self.liste_animaux_zoo.append(elm_Animal)
+        
+    def add_animal(self, animal):#ajouter un objet de type Animal à une instance de Zoo.
+        self.liste_animaux_zoo.append(animal)
+          
+          
 
-    def __str__(self):
-        return f"L animal pèse {self.poids}kg et et à une taille de {self.__taille}"
+    # Operator overloading: rajouter un support pour l’opération “+”entre objets de type Zoo.
+    def __add__(self, other):        
+        if isinstance(other, Zoo):            
+            return Zoo(self.liste_animaux_zoo+other.liste_animaux_zoo)       
+        else:           
+            return None
 
 # PROGRAMME PRINCIPAL
 chat = Animal(poids=5, taille=10)
 chien = Animal(poids=4,taille=6)
 lapin = Animal(poids=1,taille=4)
+tigre = Animal(poids=100,taille=3)
+lion = Animal(poids=150,taille=4)
 
-liste_animaux_zoo = [chat, chien]
+zoo_domestique = Zoo([chat, chien])
+zoo_domestique.add_animal(lapin)
+#liste_animaux_zoo3=
+#print(len(zoo_domestique.liste_animaux_zoo))
+#print('\n'. join ([str(i) for i in zoo_domestique.liste_animaux_zoo]))
 
-liste_animaux_zoo.append(lapin)
-print(liste_animaux_zoo)
-
-
-
-
+    
+zoo_domestique = Zoo([chat, chien])
+zoo_sauvage = Zoo([tigre, lion])
+zoo=zoo_domestique.__add__(zoo_sauvage)
+print('\n'. join ([str(i) for i in zoo.liste_animaux_zoo]))
+#print(chat)
 #animals = ['cat', 'dog', 'rabbit']
 #animals.append('guinea pig')
 #print('Updated animals list: ', animals)
